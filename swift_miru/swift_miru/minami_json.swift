44771e1464870d5e3993bc34d77417fa9154b6b2
SeikaHirori <SeikaHirori@outlook.com>
2023-04-15 13:44:36 -0700

    Only property data struct "animeSeason" is broken

--- swift_miru/swift_miru/minami_json.swift
+++ swift_miru/swift_miru/minami_json.swift
@@ -21,16 +21,16 @@ struct OfflineMinamiDatabase:Codable {
     
     struct AnimeEntry:Codable {
         let sources: [String]
-//        let title: String
-//        let type: String
-//        let episodes: Int
-//        let status: String
-//        let animeSeason: AnimeSeason
-//        let picture: String
-//        let thumbnail: String
-//        let synonyms: [String]
-//        let relations: [String]
-//        let tags: [String]
+        let title: String
+        let type: String
+        let episodes: Int
+        let status: String
+//        let animeSeason: AnimeSeason // Broken
+        let picture: String
+        let thumbnail: String
+        let synonyms: [String]
+        let relations: [String]
+        let tags: [String]
         
         
         struct AnimeSeason: Codable {
