8c1df90de4306a8129f337bd38e5b36b22a2a473
SeikaHirori <SeikaHirori@outlook.com>
2023-04-15 11:59:38 -0700

    Moved AnimeEntry into its own module

---
+++ swift_miru/swift_miru/AnimeEntry.swift
@@ -0,0 +1,27 @@
+//
+//  AnimeEntry.swift
+//  swift_miru
+//
+//  Created by Seika Hirori on 4/15/23.
+//
+
+import Foundation
+
+struct AnimeEntry:Codable {
+    let title: String
+    let sources: [String]
+    let type: String
+    let episodes: Int
+    let status: String
+    let animeSeason: AnimeSeason
+    let synonyms: [String]
+    let relations: [String]
+    let tags: [String]
+    
+    
+    
+    struct AnimeSeason: Codable {
+        let season: String
+        let year: Int
+    }
+}
