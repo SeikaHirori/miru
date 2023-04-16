a62aa102c8684edb9519aee3d1a978d24decbc2d
SeikaHirori <SeikaHirori@outlook.com>
2023-04-15 20:32:12 -0700

    - Moved AnimeEntry into its own module

--- swift_miru/swift_miru/AnimeEntry.swift
+++ swift_miru/swift_miru/AnimeEntry.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct AnimeEntry:Codable {
+    
+    let sources: [String]
+    let title: String
+    let type: String
+    let episodes: Int
+    let status: String
+    let animeSeason: AnimeSeason
+    let picture: String
+    let thumbnail: String
+//        let synonyms: [String]
+//        let relations: [String]
+    let tags: [String]
+
+}
+
+struct AnimeSeason: Codable {
+    let season: String?
+    let year: Int?
+}
--- swift_miru/swift_miru/minami_json.swift
+++ swift_miru/swift_miru/minami_json.swift
@@ -23,25 +23,6 @@ struct OfflineMinamiDatabase:Codable {
 
 }
 
-struct AnimeEntry:Codable {
-    
-    let sources: [String]
-    let title: String
-    let type: String
-    let episodes: Int
-    let status: String
-    let animeSeason: AnimeSeason
-    let picture: String
-    let thumbnail: String
-//        let synonyms: [String]
-//        let relations: [String]
-    let tags: [String]
-    
-    struct AnimeSeason: Codable {
-        let season: String?
-        let year: Int?
-    }
-}
 
 /// Offline Anime database comes from repo: https://github.com/manami-project/anime-offline-database
 /// - Returns: URL of database
